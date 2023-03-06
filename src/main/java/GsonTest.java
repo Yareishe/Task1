71b2f9d3db6eb968ad25a5b12bdc47d3a72d9472
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-03-07 00:30:56 +0200

    Update GsonTest.java

--- src/main/java/GsonTest.java
+++ src/main/java/GsonTest.java
@@ -10,7 +10,6 @@ class GsonTest {
         try (BufferedReader br = new BufferedReader(new FileReader("words.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                // process the line.
                 values.add(line);
             }
             System.out.println(values);
@@ -50,18 +49,6 @@ class GsonTest {
         }
         result.append("]");
         result = new StringBuilder(result.toString().replace(",\n]", "\n]"));
-//        String[] values1 = values.get(1).split(" ");
-//        String[] values2 = values.get(2).split(" ");
-//        result = "[\n" +
-//                "    {\n" +
-//                "        \"" + titles[0] + "\": \"" + values1[0] + "\",\n" +
-//                "        \"" + titles[1] + "\": " + values1[1] + "\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "        \"" + titles[0] + "\": \"" + values2[0] + "\",\n" +
-//                "        \"" + titles[1] + "\": " + values2[1] + "\n" +
-//                "    }\n" +
-//                "]";
         try (FileWriter writer = new FileWriter("user.json", false)) {
             writer.write(result.toString());
 
