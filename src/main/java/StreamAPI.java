f62189569c37867b5a1574a57f48b5b1365dcc04
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-03-17 19:35:01 +0200

    Update StreamAPI.java

--- src/main/java/StreamAPI.java
+++ src/main/java/StreamAPI.java
@@ -7,33 +7,34 @@ import java.util.stream.Collectors;
 class StreamAPI {
     public static void main(String[] args) {
         String[] name = {"g","b","a","k","l"};
-        String[] ABC = new StreamAPI().ABC(name).toArray(new String[0]);
-        String[] number = {"1, 2, 0", "4, 5"};
+        String[] arr = new StreamAPI().arr(name).toArray(new String[0]);
+        String[] number = {"91, 2, 0", "12, 4, 5"};
         String numbers = new StreamAPI().numbers(number);
-        System.out.println(Arrays.toString(ABC));
+        System.out.println(Arrays.toString(arr));
         System.out.println(numbers);
         formula(1234L).limit(10).forEach(System.out::println);
         Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5);
         Stream<Integer> stream2 = Stream.of(6, 7, 8, 9, 10, 11, 12, 13, 14);
         zip(stream1, stream2).forEach(System.out::println);
         }
-
-    public List<String> ABC(String[] odd){
+    public List<String> arr (String[] odd){
         List<String> names = Arrays.asList(odd);
-        List<String> sortedUpperNames = names.stream()
+        List<String> arrevers = names.stream()
                 .filter(n -> names.indexOf(n) % 2 == 0)
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
-        return sortedUpperNames;
+        return arrevers;
     }
     public String numbers(String[] odd){
-        String sortedNumbers = Arrays.stream(odd)
-                .flatMap(s -> Arrays.stream(s.split(",\\s*")))
-                .map(String::trim)
+        String result = Arrays.stream(odd)
+                .flatMap(s -> Arrays.stream(s.split(", ")))
+                .map(Integer::parseInt)
+                .distinct()
                 .sorted()
+                .map(String::valueOf)
                 .collect(Collectors.joining(", "));
-
-        return sortedNumbers;
+        System.out.println(result);
+        return result;
     }
     public static Stream<Long> formula(long seed) {
         long c = 11L;
