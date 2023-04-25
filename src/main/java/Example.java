0e83d6e302a23b7d3fc595a17d2614b85cb65462
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-04-25 21:32:38 +0300

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -11,20 +11,73 @@ public class Example {
     private static final String TEST_URL = "https://jsonplaceholder.typicode.com/users";
 
     public static void main(String[] args) throws IOException {
-        getasksforuser("1");
+        getTasksForUser("1");
     }
 
+    static class Todo {
+        private int id;
+        private int userId;
+        private String title;
+        private boolean completed;
 
-    private static  Gson  getasksforuser(String userId) throws IOException {
+        public int getId() {
+            return id;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public boolean getCompleted() {
+            return completed;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setUserId(int userId) {
+            this.userId = userId;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
+
+        @Override
+        public String toString() {
+            return "Post{" +
+                    "userId=" + userId +
+                    ", id=" + id +
+                    ", title=" + title +
+                    ", completed=" + completed + '\'' +
+                    '}';
+        }
+    }
+
+    private static  Todo[]  getTasksForUser(String userId) throws IOException {
         URL url = new URL(TEST_URL +"/" +userId +"/todos?completed=false" );
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         int responseCode = connection.getResponseCode();
         System.out.println("GET response code: " + responseCode);
+        BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String responseBody = in.lines().collect(Collectors.joining());
+        Todo[] todos = gson.fromJson(responseBody, Todo[].class);
         if (responseCode != HttpURLConnection.HTTP_OK) {
             System.out.println("not work updateUser");
         }
-        return gson;
+        return todos;
     }
 }
