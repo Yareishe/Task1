05d19fbb32965b961d228bb897bfc147e7838bea
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-05-08 22:06:47 +0300

    Create MyHashMap.java

---
+++ src/main/java/MyHashMap.java
@@ -0,0 +1,123 @@
+public class MyHashMap<K, V> {
+    public static void main(String[] args) {
+        MyHashMap<Integer, String> deliveryCities = new MyHashMap<>();
+        deliveryCities.clear();
+        deliveryCities.put(0, "Cekago");
+        deliveryCities.put(1, "Cekago1");
+        deliveryCities.put(2, "Cekago2");
+        deliveryCities.put(3, "Cekago3");
+        deliveryCities.put(1, "Cekago4");
+
+        deliveryCities.put(5, "Cekago5");
+
+        System.out.println(deliveryCities.size());
+        System.out.println(deliveryCities.toString());
+    }
+    class Node {
+        Node next;
+        K key;
+        V value;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "{"+ key  + "="+ value +',' + next + "}";
+        }
+    }
+
+    Node root = new Node(null, null);
+
+    public boolean put(K key, V value) {
+        Node newNode = new Node(key, value);
+        Node last = root;
+        if(!containsKey(key)) {
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        } else {
+            getNode(key).value = value;
+        }
+        return true;
+    }
+
+    public boolean remove(K key) {
+        Node nodeClone = new Node(null);
+        Node last = root;
+        int i = 0;
+        if(!containsKey(key)) {
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = nodeClone;
+        } else {
+            getNode(key).value = null;
+        }
+
+        return true;
+    }
+
+    void clear() {
+        root = new Node(null);
+    }
+
+    int size() {
+        Node node = root;
+        int size = 0;
+        while (node.next != null) {
+                size++;
+                node = node.next;
+        }
+        return size;
+    }
+
+    boolean containsKey(K key){
+        Node node = root;
+        boolean result = false;
+        while (node.next != null) {
+            node = node.next;
+            if(node.key.equals(key)){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    Node getNode(K key){
+        Node node = root;
+        Node result = null;
+        while (node.next != null) {
+            node = node.next;
+            if(node.key.equals(key)){
+                result = node;
+                break;
+            }
+        }
+        return result;
+    }
+
+    V get(Object key) {
+        Node node = root;
+        V result = null;
+        while (node.next != null) {
+            node = node.next;
+            if(node.key.equals(key)){
+                result = node.value;
+                break;
+            }
+        }
+        return result;
+    }
+}
