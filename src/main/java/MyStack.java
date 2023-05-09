9712f10a5a73fcb0669d0c8eb411a8e080853247
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-05-10 02:25:38 +0300

    Update MyStack.java

--- src/main/java/MyStack.java
+++ src/main/java/MyStack.java
@@ -6,25 +6,25 @@ public class MyStack<T> {
 
     public static void main(String[] args) {
         MyStack<String> deliveryCities = new MyStack<>();
-        deliveryCities.add("Cekago0");
-        deliveryCities.add("Cekago1");
-        deliveryCities.add("Cekago2");
-        deliveryCities.add("Cekago3");
-        deliveryCities.add("Cekago4");
-        deliveryCities.add("Cekago5");
-        deliveryCities.add("Cekago6");
-        deliveryCities.add("Cekago7");
-        deliveryCities.add("Cekago8");
-        deliveryCities.add("Cekago9");
-        deliveryCities.remove(6);
-        deliveryCities.pop();
+        deliveryCities.push("Cekago0");
+        deliveryCities.push("Cekago1");
+        deliveryCities.push("Cekago2");
+        deliveryCities.push("Cekago3");
+        deliveryCities.push("Cekago4");
+        deliveryCities.push("Cekago5");
+        deliveryCities.push("Cekago6");
+        deliveryCities.push("Cekago7");
+        deliveryCities.push("Cekago8");
+        deliveryCities.push("Cekago9");
+        deliveryCities.remove(0);
+        System.out.println(deliveryCities.pop());
         System.out.println(deliveryCities.peek());
         System.out.println(deliveryCities);
         deliveryCities.clear();
         System.out.println(deliveryCities.size());
     }
 
-    void add(T value) {
+    void push(T value) {
         if (array.length - 1 == filledCount) {
             Object[] array2 = new Object[array.length + 10];
             System.arraycopy(array, 0, array2, 0, array.length);
@@ -43,21 +43,22 @@ public class MyStack<T> {
     }
 
     void clear() {
-        array = new Object[filledCount];
+        array = new Object[11];
+        filledCount = 0;
     }
     int size() {
         return filledCount;
     }
 
     T peek() {
-        return (T) array[0];
+        return (T) array[array.length - 1];
     }
 
     T pop() {
-        T first = (T) array[0];
-        Object[] result = new Object[array.length - 2];
-        System.arraycopy(array, 0, result, 0, 0);
-        System.arraycopy(array, 0 + 1, result, 0, array.length - 0 - 2);
+        T first = (T) array[array.length - 2];
+        Object[] result = new Object[array.length  - 2];
+        System.arraycopy(array, 0, result, 0, array.length  - 2);
+        System.arraycopy(array, array.length  - 2 + 1, result, array.length  - 2, array.length - (array.length  - 2) - 2);
         array = result;
         filledCount--;
         return first;
