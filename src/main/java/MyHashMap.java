1d8b90afaf8d12b356bd6b989c58049ef5ad5b9d
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-05-11 00:28:40 +0300

    Create MyHashMap.java

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,14 +1,11 @@
 import java.util.Arrays;
-public class MyHashMap {
-    private static final int filledCount = 16;
-    private Node[] array;
+
+public class MyHashMap<K, V> {
+    private Node<K, V>[] table;
     private int size;
-    public MyHashMap() {
-        this.array = new Node[filledCount];
-        this.size = 0;
-    }
+
     public static void main(String[] args) {
-        MyHashMap map = new MyHashMap();
+        MyHashMap<Object,Object> map = new MyHashMap<>();
         map.put("key1", "value1");
         map.put("key2", "value2");
         map.put("key3", "value3");
@@ -19,91 +16,91 @@ public class MyHashMap {
         map.clear();
         System.out.println(map.size());
     }
-    private static class Node {
-        Object key;
-        Object value;
-        Node next;
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
 
-        public Node(Object key, Object value) {
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
-    public void put(Object key, Object value) {
-        if (key == null) {
-            throw new IllegalArgumentException("Ключ не може бути null");
-        }
-        int hash = key.hashCode();
-        int index = hash % array.length;
-        for (Node node = array[index]; node != null; node = node.next) {
+
+    public MyHashMap() {
+        table = new Node[16];
+    }
+
+    public void put(K key, V value) {
+        int hash = key.hashCode() % table.length;
+        Node<K, V> node = table[hash];
+        while (node != null) {
             if (node.key.equals(key)) {
                 node.value = value;
                 return;
             }
+            node = node.next;
         }
-        Node newNode = new Node(key, value);
-        newNode.next = array[index];
-        array[index] = newNode;
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = table[hash];
+        table[hash] = newNode;
         size++;
-        if (size > array.length * 0.75) {
+        if ((double) size / table.length > 0.75) {
             resize();
         }
     }
-    public Object get(Object key) {
-        if (key == null) {
-            throw new IllegalArgumentException("Ключ не може бути null");
-        }
-        int hash = key.hashCode();
-        int index = hash % array.length;
 
-        for (Node node = array[index]; node != null; node = node.next) {
+    public V get(K key) {
+        int hash = key.hashCode() % table.length;
+        Node<K, V> node = table[hash];
+        while (node != null) {
             if (node.key.equals(key)) {
                 return node.value;
             }
+            node = node.next;
         }
-
         return null;
     }
 
-    public void remove(Object key) {
-        if (key == null) {
-            throw new IllegalArgumentException("Ключ не може бути null");
-        }
-        int hash = key.hashCode();
-        int index = hash % array.length;
-        Node prev = null;
-        Node node = array[index];
+    public void remove(K key) {
+        int hash = key.hashCode() % table.length;
+        Node<K, V> node = table[hash];
+        Node<K, V> prevNode = null;
         while (node != null) {
             if (node.key.equals(key)) {
-                if (prev == null) {
-                    array[index] = node.next;
+                if (prevNode == null) {
+                    table[hash] = node.next;
                 } else {
-                    prev.next = node.next;
+                    prevNode.next = node.next;
                 }
                 size--;
                 return;
             }
-            prev = node;
+            prevNode = node;
             node = node.next;
         }
     }
+
     public void clear() {
-        Arrays.fill(array, null);
+        Arrays.fill(table, null);
         size = 0;
     }
+
     public int size() {
         return size;
     }
-    private void resize() {
-        Node[] oldTable = array;
-        array = new Node[oldTable.length * 2];
-        size = 0;
 
-        for (Node node : oldTable) {
+    private void resize() {
+        Node<K, V>[] newTable = new Node[table.length * 2];
+        for (Node<K, V> node : table) {
             while (node != null) {
-                put(node.key, node.value);
-                node = node.next;
+                Node<K, V> nextNode = node.next;
+                int hash = node.key.hashCode() % newTable.length;
+                node.next = newTable[hash];
+                newTable[hash] = node;
+                node = nextNode;
             }
         }
+        table = newTable;
     }
 }
